ad4d57d8fc0c8c26ee866e4641fa82afce70838c
李翌辰 <lanxan0309@gmail.com>
2013-04-01 18:42:53 +0800

    Update DBProcessor.js
    construct architecture

---
+++ public/js/DBProcessor.js
@@ -0,0 +1,15 @@
+//Data base process function
+
+//mode-idiot get/store function
+function getTodayMoneyStart(){
+    return 0;
+}
+function getTodayMoneyEnd(){
+    return 0;
+}
+function storeTodayMoneyStart(){
+    return 0;
+}
+function storeTodayMoneyEnd(){
+    return 0;
+}
