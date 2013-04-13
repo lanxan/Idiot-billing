8bb1e7f54d2a7a98f035947ac3e138571ed668f5
Mutant <evan406@hotmail.com>
2013-04-13 21:31:32 +0800

    move calculate.js to js/calculate.js

---
+++ public/js/calculate.js
@@ -0,0 +1,9 @@
+// call this js to do some calculation of money
+
+function getTodaySum(int moneyStart, int moneyEnd, int moneyPlus){
+        return (moneyStart - moneyEnd + moneyPlus);
+}
+
+function getTodaySum(int moneyStart, int moneyEnd){
+        return (moneyStart - moneyEnd);
+}
