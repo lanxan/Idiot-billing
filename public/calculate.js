974a631be8b537ccf73e8702aec06b90b7f4e0a0
Mutant <evan406@hotmail.com>
2013-04-14 12:50:32 +0800

    1. move calculate.js to js/calculate.js 2. modify index.html with inputting and calculating money

--- public/calculate.js
+++
@@ -1,9 +0,0 @@
-// call this js to do some calculation of money
-
-function getTodaySum(int moneyStart, int moneyEnd, int moneyPlus){
-        return (moneyStart - moneyEnd + moneyPlus);
-}
-
-function getTodaySum(int moneyStart, int moneyEnd){
-        return (moneyStart - moneyEnd);
-}
