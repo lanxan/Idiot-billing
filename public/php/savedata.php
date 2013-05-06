1cfc61bed5b18b012165be904f994d7379373a93
李翌辰 <lanxan0309@gmail.com>
2013-05-06 18:19:47 +0800

    Update savedata.php
    receive by post

--- public/php/savedata.php
+++ public/php/savedata.php
@@ -1,7 +1,7 @@
 <?php
 	include 'init.php';
 	
-	$_data = json_decode($data);
+	$_data = json_decode($_POST['data']);
 	mysqli_query($con, "INSERT INTO diary-billing(date, moneyStart, moneyEnd, moneyPlus, moneyTotal) 
 		VALUES($_data->{'date'}, $_data->{'moneyStart'}, $_data->{'moneyEnd'}, $_data->{'moneyPlus'}, $_data->{'moneyTotal'})");
 	
