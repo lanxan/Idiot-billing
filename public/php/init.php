94f605e3be062f88978fa0b4b736db81cf1c2328
李翌辰 <lanxan0309@gmail.com>
2013-05-11 21:04:57 +0800

    $ > &

--- public/php/init.php
+++ public/php/init.php
@@ -3,7 +3,7 @@
 	$username = "evan406@hotmail.com";
 	&password = "theones";
 	&dbname = "dbname";
-	$con = mysqli_connect($host, $username, $password, &dbname);
+	$con = mysqli_connect(&host, &username, &password, &dbname);
 	if(mysqli_connect_errno(&con)){
 		echo "Error: " . mysqli_connect_error();
 		mysqli_close($con);
