62aa97a721d98c0f846e592eafdd92a919d3547d
李翌辰 <lanxan0309@gmail.com>
2013-05-11 21:10:34 +0800

    dbname = diary-billing

--- public/php/init.php
+++ public/php/init.php
@@ -2,7 +2,7 @@
 	$host = "idiot-billing-phpmyadmin.ap01.aws.af.cm"
 	$username = "evan406@hotmail.com";
 	$password = "theones";
-	$dbname = "dbname";
+	$dbname = "diary-billing";
 	$con = mysqli_connect($host, $username, $password, $dbname);
 	if(mysqli_connect_errno($con)){
 		echo "Error: " . mysqli_connect_error();
