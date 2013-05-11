feaa8b45634538c32be77679cfa4eb5606232632
李翌辰 <lanxan0309@gmail.com>
2013-05-11 20:58:08 +0800

    Create test.php

---
+++ public/php/test.php
@@ -0,0 +1,8 @@
+<?php
+    include 'init.php';
+
+	mysqli_query($con, "INSERT INTO diary-billing(moneyStart, moneyEnd, moneyPlus, moneyTotal) 
+		VALUES($_POST['moneyStart'], $_POST['moneyEnd'], $_POST['moneyPlus'], $_POST['moneyTotal'])");
+
+	mysqli_close($con);
+?>
