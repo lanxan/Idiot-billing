94b1324240be3c766750b6ebf7e0cae5e9a6a77c
李翌辰 <lanxan0309@gmail.com>
2013-05-11 21:23:08 +0800

    include()

--- public/php/test.php
+++ public/php/test.php
@@ -1,5 +1,5 @@
 <?php
-    include 'init.php';
+    include ('init.php');
 
 	mysqli_query($con, "INSERT INTO diary-billing(moneyStart, moneyEnd, moneyPlus, moneyTotal) 
 		VALUES($_POST['moneyStart'], $_POST['moneyEnd'], $_POST['moneyPlus'], $_POST['moneyTotal'])");
